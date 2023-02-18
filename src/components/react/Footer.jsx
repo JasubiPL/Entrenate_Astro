16681ce7064bace7e730446d29be164d54d32acd
JasubiP <gameportal7@hotmail.com>
2023-02-18 14:14:36 -0600

    Change footer style

--- src/components/react/Footer.jsx
+++ src/components/react/Footer.jsx
@@ -1,16 +1,19 @@
 export default function Footer(){
   return(
-    <footer className="bg-personal-black flex justify-between p-2 items-bottom align-bottom w-full lg:h-14">
-      <div>
-      <a href="https://www.facebook.com/jasubip" target='_blank' className="flex items-center h-1/3 lg:h-auto">
-        <h3 className="text-white mr-2">By</h3>
-        <img src='/img/icons/Imagotipo-Blanco.png' alt='By JasubiP' className="h-6" />
-      </a>
+    <footer className="bg-personal-black flex flex-col px-2 py-20 align-bottom justify-center w-full">
+      <div className="flex gap-2 justify-between">
+        <h1 className="text-white text-center text-xl">Pineyro Fit</h1>
+        <div className="flex gap-2 justify-center">
+          <a href="#" target='_blank'><img src="/img/icons/facebook.png" alt="Facebook" className="h-6"/></a>
+          <a href="#" target='_blank'><img src="/img/icons/instagram.png" alt="Instagram" className="h-6" /></a>
+          <a href="#" target='_blank'><img src="/img/icons/twitter.png" alt="Twitter" className="h-6" /></a>
+        </div>
       </div>
-      <div className="flex gap-2">
-        <a href="#" target='_blank'><img src="/img/icons/facebook.png" alt="Facebook" className="h-6"/></a>
-        <a href="#" target='_blank'><img src="/img/icons/instagram.png" alt="Instagram" className="h-6" /></a>
-        <a href="#" target='_blank'><img src="/img/icons/twitter.png" alt="Twitter" className="h-6" /></a>
+      <div className="mt-3">
+        <a href="https://www.facebook.com/jasubip" target='_blank' className="flex justify-center opacity-40 hover:opacity-100">
+          <h3 className="text-white mr-2">By</h3>
+          <img src='/img/icons/Imagotipo-Blanco.png' alt='By JasubiP' className="h-8" />
+        </a>
       </div>
     </footer>
   )
