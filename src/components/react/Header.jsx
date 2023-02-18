6839f8a848c91dd7c488dc3e240a868befaf7e2c
JasubiP <gameportal7@hotmail.com>
2023-02-18 12:21:12 -0600

    Se crea Heaeder

---
+++ src/components/react/Header.jsx
@@ -0,0 +1,14 @@
+export default function Header(){
+
+  let userName = 'David'
+
+  return(
+    <header className="bg-personal-black flex p-1 justify-between items-center h-14 xl:px-4">
+      <div className="flex items-center">
+        <img src='/img/icons/logo.png' alt='logoIcon' className="h-8 align-middle"/>
+        <h1 className="ml-3 text-2xl flex h-50 text-white" >Hola<span className="text-green-500 ml-2">{userName}</span></h1>
+      </div>
+      <img src={`/img/profile-photo/${userName}.png`} alt={userName} className="rounded-full h-10 align-middle"/>
+    </header>
+  )
+}
