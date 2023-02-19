d05047e56d0293ff63723e02c3fb840d18176380
JasubiP <gameportal7@hotmail.com>
2023-02-19 12:26:29 -0600

    change icons png to svg react components

--- src/components/react/Footer.jsx
+++ src/components/react/Footer.jsx
@@ -1,12 +1,22 @@
+import FacebookIcon from "../icons/FacebookIcon";
+import InstagramIcon from "../icons/InstagramIcon";
+import TwitterIcon from "../icons/TwitterIcon";
+
 export default function Footer(){
   return(
     <footer className="bg-personal-black flex flex-col px-2 py-20 align-bottom justify-center w-full">
       <div className="flex gap-2 justify-between">
         <h1 className="text-white text-center text-xl">Pineyro Fit</h1>
-        <div className="flex gap-2 justify-center">
-          <a href="#" target='_blank'><img src="/img/icons/facebook.png" alt="Facebook" className="h-6"/></a>
-          <a href="#" target='_blank'><img src="/img/icons/instagram.png" alt="Instagram" className="h-6" /></a>
-          <a href="#" target='_blank'><img src="/img/icons/twitter.png" alt="Twitter" className="h-6" /></a>
+        <div className="flex gap-5 justify-center">
+          <a href="#" target='_blank'>
+            <FacebookIcon width='24' heigth='24' />
+          </a>
+          <a href="#" target='_blank'>
+            <InstagramIcon width='24' heigth='24' />
+          </a>
+          <a href="#" target='_blank'>
+            <TwitterIcon width='24' heigth='24' />
+          </a>
         </div>
       </div>
       <div className="mt-3">
--- src/components/react/Header.jsx
+++ src/components/react/Header.jsx
@@ -1,13 +1,21 @@
+import GymIcon from "../icons/GymIcon"
+
 export default function Header(){
 
   let userName = 'Jasubi'
 
   return(
     <header className="bg-personal-black flex p-1 justify-between items-center h-14 xl:px-4">
+      {/* Logo and user name */}
       <div className="flex items-center">
-        <img src='/img/icons/logo.png' alt='logoIcon' className="h-8 align-middle"/>
+        <GymIcon width='40' heigth='40' fill='#fff'/>
         <h1 className="ml-3 text-2xl flex h-50 text-white" >Hola<span className="text-green-500 ml-2">{userName}</span></h1>
       </div>
+      {/* Menu */}
+      <nav>
+
+      </nav>
+      {/* User profile photo */}
       <img src={`/img/profile-photo/${userName}.png`} alt={userName} className="rounded-full h-10 align-middle"/>
     </header>
   )
