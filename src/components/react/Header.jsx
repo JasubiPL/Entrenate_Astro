500832b952567a0aa491787d58c3e2f57bc58de8
JasubiP <gameportal7@hotmail.com>
2023-02-19 13:56:23 -0600

    Add nav and change Header desing

--- src/components/react/Header.jsx
+++ src/components/react/Header.jsx
@@ -1,22 +1,32 @@
+import BlogIcon from "../icons/BlogIcon"
+import EatIcon from "../icons/EatIcon"
 import GymIcon from "../icons/GymIcon"
+import HomeIcon from "../icons/HomeIcon"
 
 export default function Header(){
 
   let userName = 'Jasubi'
 
   return(
-    <header className="bg-personal-black flex p-1 justify-between items-center h-14 xl:px-4">
+    <header className="bg-personal-black grid grid-rows-2 grid-cols-2 p-1 items-center h-28 lg:h-14 xl:px-4 lg:grid-rows-1 lg:grid-cols-3">
       {/* Logo and user name */}
       <div className="flex items-center">
         <GymIcon width='40' heigth='40' fill='#fff'/>
         <h1 className="ml-3 text-2xl flex h-50 text-white" >Hola<span className="text-green-500 ml-2">{userName}</span></h1>
       </div>
       {/* Menu */}
-      <nav>
-
+      <nav className="flex bg-personal-black w-full justify-center row-start-2 col-span-2 lg:row-start-1 lg:col-start-2 lg:col-span-1">
+        <ul className="flex w-full max-w-xl justify-around">
+          <a href="/"><li><HomeIcon width='26' heigth='26'/></li></a>
+          <a href="/blog"><li><BlogIcon width='26' heigth='26' /></li></a>
+          <a href="/ejercicio"><li><GymIcon width='26' heigth='26' fill='#fff'/></li></a>
+          <a href="/recetas"><li><EatIcon width='26' heigth='26' /></li></a>
+        </ul>
       </nav>
-      {/* User profile photo */}
-      <img src={`/img/profile-photo/${userName}.png`} alt={userName} className="rounded-full h-10 align-middle"/>
+      {/* User profile photo and menu 2 */}
+      <div className="flex justify-end">
+        <img src={`/img/profile-photo/${userName}.png`} alt={userName} className="rounded-full h-10 cursor-pointer"/>
+      </div>
     </header>
   )
 }
